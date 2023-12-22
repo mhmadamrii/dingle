3b37d821da8c7044ecbb5655f06eae70dfefdeab
mhmadamrii <interceptorghost4@gmail.com>
2023-12-22 18:32:04 +0700

    jfdls;kfj

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,10 +1,10 @@
-import { NextResponse } from "next/server"
+import { NextResponse } from 'next/server';
 
 export async function GET() {
-    try {
-        return NextResponse.json({ message: 'Hello world' })
-    } catch (error) {
-        console.log('error', error)
-        return new NextResponse("failed", { status: 500 })
-    }
+  try {
+    return NextResponse.json({ message: 'Hello world' });
+  } catch (error) {
+    console.log('error', error);
+    return new NextResponse('failed', { status: 500 });
+  }
 }
--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,12 +1,12 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
 declare global {
-    var prisma: PrismaClient | undefined;
-};
+  var prisma: PrismaClient | undefined;
+}
 
 // export const db = globalThis.prisma || new PrismaClient();
 
 // if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
-export const db = new PrismaClient()
+export const db = new PrismaClient();
 
 // we're not just doing export default db = new PrismaClient() because it would crash in dev mode because of hot reload
