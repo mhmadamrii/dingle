fa772103812018154104c5ab5dba7cbe4250aafe
mhmadamrii <interceptorghost4@gmail.com>
2023-12-30 00:40:36 +0700

    f;dlkjasj

---
+++ src/app/(root)/(dashboard)/teacher/courses/page.tsx
@@ -0,0 +1,14 @@
+export default function Courses() {
+  return (
+    <div>
+      <h1>Welcome back</h1>
+      <span>
+        Lorem ipsum dolor, sit amet consectetur adipisicing
+        elit. Reiciendis omnis minima voluptatum cumque,
+        aliquid ullam provident odit, unde consequatur
+        molestiae vitae ut soluta sed maxime, ea alias
+        veritatis perferendis consequuntur!
+      </span>
+    </div>
+  );
+}
